ef2bcdb98daa48e364fc6f2177de2803963b81b4
Abhijit Roy <dalvath3700@gmail.com>
2022-07-18 14:22:46 +0530

    Modify the dependencies
    - Previous authors added
    - Hardhat config file updated solidity version: `0.8.13`
    - removed the comment
    - IERC20, Ownable, Context updated to `^0.8.0`
    - build folder to be replaced by the new contract code (optimized)

--- contracts/Ownable/Context.sol
+++ contracts/Ownable/Context.sol
@@ -1,17 +1,24 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
 
-// SPDX-License-Identifier: UNLICENSED
-contract Context {
-    // Empty internal constructor, to prevent people from mistakenly deploying
-    // an instance of this contract, which should be used via inheritance.
-    constructor() internal {}
+pragma solidity ^0.8.0;
 
-    function _msgSender() internal view returns (address payable) {
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
         return msg.sender;
     }
 
-    function _msgData() internal view returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode
+    function _msgData() internal view virtual returns (bytes calldata) {
         return msg.data;
     }
 }
